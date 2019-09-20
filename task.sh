0485a7afd9a7dba5a95efd9c34daf97a344b9050
kirastain <kirastain@gmail.com>
2019-09-20 23:41:07 +0300

    done

--- task.sh
+++ task.sh
@@ -10,10 +10,10 @@ do
                         if [[ "$file" = *.MOV ]]
                         then
                                 a=$(ffprobe -i $file -show_entries format=duration -v quiet -of csv="p=0");
-                                echo $a >> ./task/task2
+                                echo $a >> $path/task/task2
                         else
                                 a=`echo None`;
-                                echo $a >> ./task/task2
+                                echo $a >> $path/task/task2
                         fi
                 fi
         fi
@@ -26,9 +26,14 @@ do
 	fi
 done
 }
+find . -type d -name task -exec rm -r {} +
+mkdir task
+path=$(pwd)
 ls -l -h -R -I "task" -I "task.sh" | awk -F" " 'NF>3{i=9; while (i<NF) {printf("%s ", $i); i++;} printf("%s\t", $i)}NF>3{printf("%s\t%s %s %s\t%s\n", $5, $6, $7, $8, $1)}' > ./task/task0
 ls -R -l -I "task" -I "task.sh" | awk -F" " 'NF>3{print $9}' | sed 's/.*\.//' > ./task/task1
 > ./task/task2
-Length $(ls ".") > ./task/task2
+Length $(ls ".")
+cd
+cd $path
 paste -d'\t' ./task/task0 ./task/task1 ./task/task2 > ./task/res.xls
 cat ./task/res.xls
