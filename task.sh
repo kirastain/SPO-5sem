60c0b0b072092a5edb039bbd49314cd34c84bce0
kirastain <kirastain@gmail.com>
2019-09-23 10:16:04 +0300

    upd2

--- task.sh
+++ task.sh
@@ -7,7 +7,7 @@ do
         then
                 if [[ "$file" != ./task.sh ]]
                 then
-                        if [[ "$file" = *.MOV ]]
+                        if [[ "$file" = *.MOV || "$file" = *.mp3 || "$file" = *.avi || "$file" = *.mp4 ]]
                         then
                                 a=$(ffprobe -i $file -show_entries format=duration -v quiet -of csv="p=0");
                                 echo $a >> $path/task/task2
