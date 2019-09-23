e487f156bd18e59857bb0567994479d670bac5e9
kirastain <kirastain@gmail.com>
2019-09-23 12:25:04 +0300

    upd4

--- task.sh
+++ task.sh
@@ -29,8 +29,8 @@ done
 find . -type d -name task -exec rm -r {} +
 mkdir task
 path=$(pwd)
-ls -l -h -R -I "task" -I "task.sh" | awk -F" " 'NF>3{i=9; while (i<NF) {printf("%s ", $i); i++;} printf("%s\t", $i)}NF>3{printf("%s\t%s %s %s\t%s\n", $5, $6, $7, $8, $1)}' > ./task/task0
-ls -R -l -I "task" -I "task.sh" | awk -F" " 'NF==9{print $9}NF>9{i=9; while (i<NF) {printf("%s ", $i); i++;} printf("%s\n", $i)}' | sed 's/.*\.//' > ./task/task1
+ls -l -R -I "task" -I "task.sh" | awk -F"\ " 'NF>3{i=9; while (i<NF) {printf("%s ", $i); i++;} printf("%s\t", $i)}NF>3{printf("%s\t%s %s %s\t%s\n", $5, $6, $7, $8, $1)}' > ./task/task0
+ls -R -l -I "task" -I "task.sh" | awk -F" " 'NF==9{print $9}NF>9{i=9; while (i<NF) {printf("%s ", $i); i++;} printf("%s\n", $i)}'| sed 's/.*\.//' > ./task/task1
 Length $(ls ".")
 cd
 cd $path
