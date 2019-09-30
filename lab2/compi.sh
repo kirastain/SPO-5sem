5363c1be6cccf29e9e5eb90278e75fce33443baf
kirastain <kirastain@gmail.com>
2019-09-30 20:57:50 +0300

    lab2

---
+++ lab2/compi.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/andrejv/maxima
+cd Maxima
+sudo apt-get install g++-mingw-w64-i686 cmake nsis wine automake texinfo rsync p7zip-full tcl-dev tk-dev texlive g++ libgl1-mesa-dev gettext
+cd crosscompile-windows/build # change to the build directory
+cmake ..
+make
+make package
+cd ..
+cd ..
+cd ..
+mv ./Maxima /tmp
