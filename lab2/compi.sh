e4acdf2516bcf64cb791712cf8b57a1b2f578949
kirastain <kirastain@gmail.com>
2019-10-01 15:09:17 +0300

    upd

--- lab2/compi.sh
+++ lab2/compi.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 git clone https://github.com/andrejv/maxima
 cd Maxima
 sudo apt-get install g++-mingw-w64-i686 cmake nsis wine automake texinfo rsync p7zip-full tcl-dev tk-dev texlive g++ libgl1-mesa-dev gettext
